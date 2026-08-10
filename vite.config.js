37f73936afed7f929aacabec33fc69d4ed91f534
ptljenil360-ops <ptljenil360@gmail.com>
2026-08-10 07:05:10 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-    base: "/",
+    base: "/rocksoul/",
     plugins:[
         react(),
         tailwind(),
